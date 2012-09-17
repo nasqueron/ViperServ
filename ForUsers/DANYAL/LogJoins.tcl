bd0a0d05ec2ede5a9935e80dc54511ecb582c6d7
Dereckson <dereckson@espace-win.org>
2012-09-17 22:30:52 +0200

    New TCL for DANYAL: LogJoins.tcl

---
+++ ForUsers/DANYAL/LogJoins.tcl
@@ -0,0 +1,46 @@
+# This TCL for an oper eggdrop logs "Client connecting" lines.
+
+#
+# Configuration
+#
+
+set LogJoins(file) join.log
+set LogJoins(server) Irc.ApniISP.CoM
+
+#
+# Events
+#
+
+bind raw  - NOTICE   raw:logjoin
+
+#Handles server notices
+proc raw:logjoin {from keyword text} {
+	global LogJoins
+	if {$from == $LogJoins(server) && $keyword == "NOTICE"} {
+		set pos [string first "Client connecting" $text]
+		if {$pos > -1} {
+			log_entry $LogJoins(file) [string range $text $pos end]
+			putdebug "°°° Notice logged"
+		} {
+			putdebug "°°° Notice discarded (not connect)"
+		}
+	} {
+		putdebug "°°° Notice discarded (bad from/keyword : $from $keyword)"
+	}
+}
+
+#
+# Helper methods
+#
+
+#Returns a log message, prepended by current time
+proc log_message {message} {
+	return "[clock format [unixtime] -format "%x %X"] $message"
+}
+
+#Logs a message in the specified file
+proc log_entry {file message} {
+	set    fd [open $file a]
+	puts  $fd [log_message $message]
+	close $fd
+}
