7d38eb85e6722f0a753a3544864ae3a224a4c410
Dereckson <dereckson@espace-win.org>
2013-01-11 16:57:09 +0100

    Draft: Bugzilla script

---
+++ Nasqueron/Bugzilla.tcl
@@ -0,0 +1,66 @@
+# .tcl source scripts/Nasqueron/Bugzilla.tcl
+
+package require SOAP
+package require rpcvar
+
+package require http
+package require tls
+http::register https 443 ::tls::socket
+
+bind dcc - bug dcc:bug
+
+#
+# Bugzilla structures
+#
+
+namespace import -force rpcvar::typedef
+
+typedef {
+   login	string
+   password	string
+   remember	boolean
+} userLoginRequest
+
+#
+# Bugzilla libraries
+#
+
+namespace eval ::Bugzilla:: {
+	proc endpoint {server} {
+		return [registry get bugzilla.$server.url]/xmlrpc.cgi
+	}
+
+	proc login {server} {
+		global errorInfo
+		if [catch { ::Bugzilla::${server}::UserLogin [list \
+			login		[registry get bugzilla.$server.username] \
+			password	[registry get bugzilla.$server.password] \
+			remember	1 \
+		] } reply] {
+			error [lindex [split $errorInfo \n] 0]
+		}
+		return $reply
+	}
+
+	proc version {server} {
+		::Bugzilla::${server}::BugzillaVersion
+	}
+}
+
+#
+# XML-RPC procedures
+#
+
+foreach bzServer [registry get bugzilla.servers] {
+	namespace eval ::Bugzilla::${bzServer} { }
+	XMLRPC::create ::Bugzilla::${bzServer}::UserLogin -name "User.login" -proxy [Bugzilla::endpoint $bzServer] -params {login userLoginRequest}
+	XMLRPC::create ::Bugzilla::${bzServer}::BugzillaVersion -name "Bugzilla.version" -proxy [Bugzilla::endpoint $bzServer]
+}
+
+#
+# Userland
+#
+
+proc dcc:bug {handle idx arg} {
+	
+}
