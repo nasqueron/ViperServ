5c060931e605005b116500f5862ac91648974bd4
Sébastien Santoro <dereckson@espace-win.org>
2016-06-04 00:52:13 +0000

    Notifications: send TrustSpace notifications to #wolfplex
    Summary: Ref. T850.
    
    Test Plan: Fire a notification for TrustSpace
    
    Reviewers: dereckson
    
    Subscribers: ledesillusionniste
    
    Maniphest Tasks: T850
    
    Differential Revision: http://devcentral.nasqueron.org/D393

--- Wearg/Notifications.tcl
+++ Wearg/Notifications.tcl
@@ -73,6 +73,9 @@ namespace eval notifications {
 		if {$project == "Wolfplex"} {
 			return "#wolfplex"
 		}
+		if {$project == "TrustSpace"} {
+			return "#wolfplex"
+		}
 		if {$project == "Keruald"} {
 			return "#nasqueron-logs"
 		}
