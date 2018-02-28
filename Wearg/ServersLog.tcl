64695d124d3830f9f3a73ee1c307771693ecedd6
Sébastien Santoro <dereckson@espace-win.org>
2018-02-28 22:22:53 +0000

    Allow [Foo] Done something syntax for servers log
    Summary:
    Log entries syntax is "[component] message".
    
    If a component is known, such syntax is directly allowed.
    If a component is unknown, the user is invited to prepend .+log.
    
    Test Plan:
    Tested with `21:44 < Dereckson> [Eglide] Prune libapache2-mod-php7.1`.
    
    Unit tests are provided for `is_known_component`.
    
    Reviewers: dereckson
    
    Reviewed By: dereckson
    
    Differential Revision: https://devcentral.nasqueron.org/D1366

--- Wearg/ServersLog.tcl
+++ Wearg/ServersLog.tcl
@@ -3,6 +3,19 @@ package require rest
 bind pub -  .+log       pub:log
 bind dcc -   +log       dcc:log
 
+bind pubm -  "#* \[*\] *"        pubm:log
+
+proc pubm:log {nick uhost handle chan text} {
+    regexp "\\\[(.*)\\\] (.*)" $text match component entry
+
+    if {[is_known_component $component]} {
+        add_to_servers_log $emitter "$network $source" $component $entry
+        putcmdlog "<<$nick>> !$handle! .+log $text"
+    } {
+        putserv "PRIVMSG $chan :$nick, if you wish to log that, confirm with .+log $text"
+    }
+}
+
 proc pub:log {nick uhost handle chan arg} {
     set callback [get_putbymode_chan_callback $chan $nick]
     handle_send_to_servers_log [resolve_nick $nick] $chan $arg $callback
@@ -11,6 +24,20 @@ proc pub:log {nick uhost handle chan arg} {
 proc dcc:log {handle idx arg} {
     global username
     handle_send_to_servers_log $handle $username $arg "dcc $idx"
+}
+
+proc is_known_component {candidate} {
+    # If "Dwellers" is a known component, are known:
+    #   - Dwellers
+    #   - Dwellers/DevCentral
+
+    foreach component [registry get serverslog.knowncomponents] {
+        if {[regexp ^${component}(/.+)?$ $candidate]} {
+            return 1
+        }
+    }
+
+    return 0
 }
 
 proc handle_send_to_servers_log {emitter source arg callback} {
